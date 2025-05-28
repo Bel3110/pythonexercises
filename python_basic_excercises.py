bae54246ecd9c8f56c914155e40c60b479d4876f
Belen Carmona <98711618+Bel3110@users.noreply.github.com>
2025-05-28 16:54:20 -0300

    Add files via upload
    ejercicios 1 a 7

---
+++ python_basic_excercises.py
@@ -0,0 +1,26 @@
+# 1. Formatted twinke poem
+#print('Twinkle twinkle little star \n\tHow i wonder what you are \n\t\tUp above the world so high \n\t\tLike a diamond in the sky \nTwinkle twinkle little star \n\tHow i wonder what you are')
+
+# 2. Python version checker
+
+# 3. Current time and date display
+
+# 4. Circle area calculator
+#radio = float(input('Ingrese el radio del circulo: '))
+#print('El area del circulo es ', 3.14159265 * radio**2 )
+
+# 5. Reverse full name
+#nombre = input('Ingrese su nombre: ')
+#apellido = input('Ingrese su apellido: ') 
+#print(apellido, nombre)
+
+# 6. Lists and tuples
+#values = input('Ingrese una serie de numeros separados por comas: ')
+#separados = values.split(",")
+#lista = [separados]
+#tupla = tuple(lista)
+#print(lista)
+#print(tupla)
+
+# 7. File extension extractor
+filename = input('Ingrese el nombre de un archivo: ')
